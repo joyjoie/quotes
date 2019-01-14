68cb576dbf0a188ebff7a7876d66f645d7b74500
joy <wendokingoo@gmail.com>
2019-01-14 18:56:01 +0300

    array element modified

--- quotes/src/app/quote-description/quote-description.component.ts
+++ quotes/src/app/quote-description/quote-description.component.ts
@@ -10,12 +10,12 @@ export class QuoteDescriptionComponent implements OnInit {
 
   quotes =[
      new Quote (1,'Monday','My life is my mine',' written by abby',new Date(2018,3,14),'submitted by lola'),
-     new Quote (2,'Tuesday','My life is my mine',' written by abby',new Date(2018,6,9) ,"submitted by lilo"),
-     new Quote (3,'Wednesday','My life is my mine',' written by me',new Date(2018,7,12),"submitted by lilo"),
-     new Quote (4,'Life','My life is my mine',' written by ivy',new Date(2018,4,18),"submitted by lilo"),
-     new Quote (5,'Life','My life is my mine',' written by caro',new Date(2018,2,14),"submitted by lilo"),
-     new Quote (6,'Life','My life is my mine',' written by abby',new Date(2018,11,14),"submitted by lilo"),
-     new Quote (7,'Life','My life is my mine',' written by litty',new Date(2019,0,10),"submitted by lilo"),
+     new Quote (2,'Tuesday','Confidence is awesome',' written by abby',new Date(2018,6,9) ,"submitted by lilo"),
+     new Quote (3,'Wednesday','Do not go to bed angry',' written by me',new Date(2018,7,12),"submitted by lilo"),
+     new Quote (4,'Thursday','Pizza melts hearts ',' written by ivy',new Date(2018,4,18),"submitted by lilo"),
+     new Quote (5,'Friday','Walking everyday keeps you away from the doctor,maybe',' written by caro',new Date(2018,2,14),"submitted by lilo"),
+     new Quote (6,'Saturday','My life is my mine',' written by abby',new Date(2018,11,14),"submitted by lilo"),
+     new Quote (7,'Sunday','My life is my mine',' written by litty',new Date(2019,0,10),"submitted by lilo"),
    ]
 
   toggleDetails(isComplete,index) {
