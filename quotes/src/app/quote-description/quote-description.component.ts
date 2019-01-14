832081178e6c7fec3e301a1751f98b539c06451b
joy <wendokingoo@gmail.com>
2019-01-14 12:48:25 +0300

    like buton added

--- quotes/src/app/quote-description/quote-description.component.ts
+++ quotes/src/app/quote-description/quote-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-description',
@@ -6,36 +6,30 @@ import { Quote } from '../quote'
   styleUrls: ['./quote-description.component.css']
 })
 export class QuoteDescriptionComponent implements OnInit {
-
+@Output () addVote=new EventEmitter<vote>();
   quotes = [
     new Quote(1, 'There is nothing permanent except change.', 'Heraclitus', 'Submitted by Joie'),
     new Quote(2, 'Independence is happiness.', 'Susan B. Anthony', 'Submitted by Abby'),
     new Quote(3, 'No act of kindness, no matter how small, is ever wasted.', 'Aesop', 'Submitted by Anon'),
   ]
-  @Input() quote: Quote;
-  @Output() isComplete = new EventEmitter<boolean>();
-  // event emitter above and below
-  quoteDelete(complete: boolean) {
-    this.isComplete.emit(complete);
-  }
-  deleteQuote(isComplete, index) {
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].author}`)
 
-      if (toDelete) {
-        this.quotes.splice(index, 1)
-      }
-    }
-  }
-  toogleDetails(index) {
-    this.quotes[index].showDescription = !this.quotes[index].showDescription;
-  }
-    addNewQuote(quote){
-      let quoteLength = this.quotes.length;
-     this.quote.id=quoteLength+1;
-     quote.completeDate = new Date(quote.completeDate);
-      this.quotes.push(quote);
-      }
+
+toggleDetails(index){
+  this.quotes[index].showDescription =!this.quotes[index].showDescription;
+
+}
+deleteGoal(isComplete,index){
+if(isComplete){
+  let toDelete = confirm('are you sure you want to delete?  ${this.quotes[index].quot}')
+
+if(toDelete){
+this.quotes.splice(index,1)
+
+}
+
+}
+
+}
 
   constructor() { }
 
